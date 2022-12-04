79089d5a20d0ed02b49d087716c4e8fceacf9179
Mahdi <mahdi.akbari.72@gmail.com>
2022-12-05 00:50:40 +0330

    Design MainActivity layout

--- app/src/main/java/com/example/runningtracker/MainActivity.kt
+++ app/src/main/java/com/example/runningtracker/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.runningtracker
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.runningtracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomAppbar.background = ContextCompat.getDrawable(
+            this, R.color.background)
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(1).isEnabled = false
+
+        binding.fab.setOnClickListener {
+            Toast.makeText(this,
+                "fab clicked", Toast.LENGTH_SHORT).show()
+            Log.d("!!!!!!!!!!!!","clicked")
+        }
+        binding.bottomNavigationView.itemIconTintList = ContextCompat
+            .getColorStateList(this, R.drawable.color_state_list_resource)
     }
 }
